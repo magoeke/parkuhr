14644ae8c934ed02cc715f4944e2061bbc9ddf49
magoeke <Maximilian.Goeke@doubleslash.de>
2022-03-27 19:58:26 +0200

    Implemented functionality.

--- src/main/java/de/rwu/softwareengineering/Parkuhr.java
+++ src/main/java/de/rwu/softwareengineering/Parkuhr.java
@@ -1,11 +1,32 @@
 package de.rwu.softwareengineering;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parkuhr {
 
+    private final List<Integer> changeOptions = List.of(50, 20, 10, 5, 2, 1);
+
     public List<Integer> calculateChange(int bezahlt, int kosten) {
-        return List.of();
+        if(bezahlt < kosten) {
+            throw new IllegalStateException();
+        }
+
+        final var change = new ArrayList<Integer>();
+        var diff = bezahlt - kosten;
+        var i = 0;
+        while(diff != 0) {
+            final var changeCandidate = changeOptions.get(i);
+
+            if(changeCandidate <= diff) {
+                change.add(changeCandidate);
+                diff -= changeCandidate;
+            } else {
+                i++;
+            }
+        }
+
+        return change;
     }
 
 }
--- src/test/java/de/rwu/softwareengineering/ParkuhrTest.java
+++ src/test/java/de/rwu/softwareengineering/ParkuhrTest.java
@@ -1,14 +1,42 @@
 package de.rwu.softwareengineering;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ParkuhrTest {
 
-    void test() {
-        final var parkuhr = new Parkuhr();
+    private Parkuhr parkuhr = new Parkuhr();
+
+    @Test
+    void should_return_no_change_when_paid_amount_equals_costs() {
         final var change = parkuhr.calculateChange(0, 0);
+
         assertThat(change).isEmpty();
+    }
+
+    @Test
+    void should_throw_exception_when_too_little_was_paid() {
+        assertThatThrownBy(() -> parkuhr.calculateChange(0, 1))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void should_return_5_when_10_was_paid_and_it_costs_5() {
+        final var change = parkuhr.calculateChange(10, 5);
+
+        assertThat(change).contains(5);
+    }
+
+    @Test
+    void should_return_2_and_1_when_8_was_paid_and_it_costs_5() {
+        final var change = parkuhr.calculateChange(8, 5);
+
+        assertThat(change).contains(2, 1);
     }
 
 }
